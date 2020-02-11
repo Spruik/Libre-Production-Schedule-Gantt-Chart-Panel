8e4092c92a0b2b54db3295183fe5ca3c74baeccb
Jamen1147 <jamen1147@gmail.com>
2020-02-11 16:15:08 +1100

    fix deletion bug

--- dist/order_actions_ctrl.js
+++ dist/order_actions_ctrl.js
@@ -1,156 +1,160 @@
 'use strict';
 
 System.register(['./utils', './influx_helper', './insertion_actions_ctrl', './editing_actions_ctrl', './data_processor', './constans', 'moment', './chart_option'], function (_export, _context) {
-	"use strict";
+  "use strict";
 
-	var utils, influx, insert_actions, edit_actions, dp, cons, moment, chart, _order, _orderStates, closeForm;
+  var utils, influx, insert_actions, edit_actions, dp, cons, moment, chart, _order, _orderStates, closeForm;
 
-	function showOrderActions(order) {
-		//set the order passed in global
-		_order = order;
+  function showOrderActions(order) {
+    //set the order passed in global
+    _order = order;
 
-		//check if the order is available for editing, only 'planned' and 'ready' can be edited by scheduler
-		if (order.status !== cons.STATE_PLAN && order.status !== cons.STATE_READY) {
-			utils.alert('warning', 'Warning', 'The order status is ' + order.status + ', so it is no longer available for editing');
-			return;
-		}
+    //check if the order is available for editing, only 'planned' and 'ready' can be edited by scheduler
+    if (order.status !== cons.STATE_PLAN && order.status !== cons.STATE_READY) {
+      utils.alert('warning', 'Warning', 'The order status is ' + order.status + ', so it is no longer available for editing');
+      return;
+    }
 
-		//pop up the actions form
-		utils.showModal('order_actions.html', {});
+    //pop up the actions form
+    utils.showModal('order_actions.html', {});
 
-		//set listeners
-		removeListeners();
-		addListeners();
-	}
+    //set listeners
+    removeListeners();
+    addListeners();
+  }
 
-	_export('showOrderActions', showOrderActions);
+  _export('showOrderActions', showOrderActions);
 
-	function removeListeners() {
-		$(document).off('click', 'input[type="radio"][name="product-schedule-gantt-chart-order-actions-radio"]');
-	}
+  function removeListeners() {
+    $(document).off('click', 'input[type="radio"][name="product-schedule-gantt-chart-order-actions-radio"]');
+  }
 
-	function addListeners() {
-		$(document).on('click', 'input[type="radio"][name="product-schedule-gantt-chart-order-actions-radio"]', function (e) {
-			if (e.target.id === 'insert') {
-				insertOrder();
-			} else if (e.target.id === 'edit') {
-				editOrder();
-			} else if (e.target.id === 'release') {
-				if (_order.status === cons.STATE_READY) {
-					utils.alert('warning', 'Warning', 'Order has already been released');
-					closeForm();
-				} else {
-					updateOrderStatus(cons.STATE_READY);
-				}
-			} else if (e.target.id === 'delete') {
-				updateOrderStatus(cons.STATE_DELETED);
-			}
-		});
-	}
+  function addListeners() {
+    $(document).on('click', 'input[type="radio"][name="product-schedule-gantt-chart-order-actions-radio"]', function (e) {
+      if (e.target.id === 'insert') {
+        insertOrder();
+      } else if (e.target.id === 'edit') {
+        editOrder();
+      } else if (e.target.id === 'release') {
+        if (_order.status === cons.STATE_READY) {
+          utils.alert('warning', 'Warning', 'Order has already been released');
+          closeForm();
+        } else {
+          updateOrderStatus(cons.STATE_READY);
+        }
+      } else if (e.target.id === 'delete') {
+        updateOrderStatus(cons.STATE_DELETED);
+      }
+    });
+  }
 
-	async function insertOrder() {
-		var url = utils.postgRestHost + 'order_state';
-		var result = await utils.sure(utils.get(url));
-		if (result.ok) {
-			_orderStates = result.data;
-			insert_actions.showActions(_order);
-		} else {
-			utils.alert('error', 'Error', 'Error occurred when getting the Order State Configuration due to ' + result.error + ' please try again or contact the dev team');
-		}
-	}
+  async function insertOrder() {
+    var url = utils.postgRestHost + 'order_state';
+    var result = await utils.sure(utils.get(url));
+    if (result.ok) {
+      _orderStates = result.data;
+      insert_actions.showActions(_order);
+    } else {
+      utils.alert('error', 'Error', 'Error occurred when getting the Order State Configuration due to ' + result.error + ' please try again or contact the dev team');
+    }
+  }
 
-	async function editOrder() {
-		var url = utils.postgRestHost + 'order_state';
-		var result = await utils.sure(utils.get(url));
-		if (result.ok) {
-			_orderStates = result.data;
-			edit_actions.showActions(_order);
-		} else {
-			utils.alert('error', 'Error', 'Error occurred when getting the Order State Configuration due to ' + result.error + ' please try again or contact the dev team');
-		}
-	}
+  async function editOrder() {
+    var url = utils.postgRestHost + 'order_state';
+    var result = await utils.sure(utils.get(url));
+    if (result.ok) {
+      _orderStates = result.data;
+      edit_actions.showActions(_order);
+    } else {
+      utils.alert('error', 'Error', 'Error occurred when getting the Order State Configuration due to ' + result.error + ' please try again or contact the dev team');
+    }
+  }
 
-	function getOrderStates() {
-		return _orderStates;
-	}
+  function getOrderStates() {
+    return _orderStates;
+  }
 
-	_export('getOrderStates', getOrderStates);
+  _export('getOrderStates', getOrderStates);
 
-	function updateOrderStatus(status) {
-		var line = influx.writeLineForUpdate(status, _order);
-		if (status === 'Deleted') {
-			deleteCurrentAndUpdateAffectOrders(line);
-		} else {
-			utils.post(influx.writeUrl, line).then(function (res) {
-				utils.alert('success', 'Success', 'Order has been marked as ' + status);
-				closeForm();
-				chart.refreshDashboard();
-			}).catch(function (e) {
-				utils.alert('error', 'Database Error', 'An error occurred while updating the order : ' + e);
-				closeForm();
-			});
-		}
-	}
+  function updateOrderStatus(status) {
+    console.log('status', status);
+    var line = influx.writeLineForUpdate(status, _order);
+    if (status.toLowerCase() === 'deleted') {
+      deleteCurrentAndUpdateAffectOrders(line);
+    } else {
+      utils.post(influx.writeUrl, line).then(function (res) {
+        utils.alert('success', 'Success', 'Order has been marked as ' + status);
+        closeForm();
+        chart.refreshDashboard();
+      }).catch(function (e) {
+        utils.alert('error', 'Database Error', 'An error occurred while updating the order : ' + e);
+        closeForm();
+      });
+    }
+  }
 
-	function deleteCurrentAndUpdateAffectOrders(line) {
-		//create promises array and put the 'delete current order request' into it first
-		var promises = [utils.post(influx.writeUrl, line)];
+  function deleteCurrentAndUpdateAffectOrders(line) {
+    console.log('gantt deleteAndUpdateBehind');
+    //create promises array and put the 'delete current order request' into it first
+    var promises = [utils.post(influx.writeUrl, line)];
 
-		//get all orders data for further filtering
-		var allData = dp.getData();
+    //get all orders data for further filtering
+    var allData = dp.getData();
 
-		//filter affected orders using all orders data
-		//affected orders = order.startTime >= thisOrder.endtime && in the same line && with the same date.
-		var affectedOrders = allData.filter(function (order) {
-			return order.startTime >= _order.endTime && order.production_line === _order.production_line && order.order_date === _order.order_date;
-		});
+    //filter affected orders using all orders data
+    //affected orders = order.startTime >= thisOrder.endtime && in the same line && with the same date.
+    var affectedOrders = allData.filter(function (order) {
+      return order.startTime >= _order.endTime && order.production_line === _order.production_line && order.order_date === _order.order_date;
+    });
 
-		//work out thisOrder's total duration, which = its duration + its changeover duration
-		var deletingOrderDurationHour = moment.duration(_order.order_qty / (_order.planned_rate * 60), 'hours');
-		var deletingOrderChangeover = moment.duration(_order.planned_changeover_time, 'H:mm:ss');
-		var deletingOrderTotalDur = deletingOrderDurationHour.add(deletingOrderChangeover);
+    //work out thisOrder's total duration, which = its duration + its changeover duration
+    var deletingOrderDurationHour = moment.duration(_order.order_qty / (_order.planned_rate * 60), 'hours');
+    var deletingOrderChangeover = moment.duration(_order.planned_changeover_time, 'H:mm:ss');
+    var deletingOrderTotalDur = deletingOrderDurationHour.add(deletingOrderChangeover);
 
-		//loop affected orders, order's starttime and endtime should both subtract the total duration worked out
-		affectedOrders.forEach(function (order) {
-			var line = influx.writeLineForTimeUpdate(order, deletingOrderTotalDur, 'subtract');
-			promises.push(utils.post(influx.writeUrl, line));
-		});
+    console.log('gantt total delete dur', deletingOrderTotalDur);
 
-		Promise.all(promises).then(function () {
-			utils.alert('success', 'Success', 'Order has been marked as Deleted');
-			closeForm();
-			chart.refreshDashboard();
-		}).catch(function (e) {
-			utils.alert('error', 'Database Error', 'An error occurred while deleting the order : ' + e);
-			closeForm();
-		});
-	}
-	return {
-		setters: [function (_utils) {
-			utils = _utils;
-		}, function (_influx_helper) {
-			influx = _influx_helper;
-		}, function (_insertion_actions_ctrl) {
-			insert_actions = _insertion_actions_ctrl;
-		}, function (_editing_actions_ctrl) {
-			edit_actions = _editing_actions_ctrl;
-		}, function (_data_processor) {
-			dp = _data_processor;
-		}, function (_constans) {
-			cons = _constans;
-		}, function (_moment) {
-			moment = _moment.default;
-		}, function (_chart_option) {
-			chart = _chart_option;
-		}],
-		execute: function () {
-			_order = void 0;
-			_orderStates = void 0;
+    //loop affected orders, order's starttime and endtime should both subtract the total duration worked out
+    affectedOrders.forEach(function (order) {
+      var line = influx.writeLineForTimeUpdate(order, deletingOrderTotalDur, 'subtract');
+      promises.push(utils.post(influx.writeUrl, line));
+    });
 
-			closeForm = function closeForm() {
-				return $('a#product-schedule-gantt-chart-order-actions-close-btn').trigger('click');
-			};
-		}
-	};
+    Promise.all(promises).then(function () {
+      utils.alert('success', 'Success', 'Order has been marked as Deleted');
+      closeForm();
+      chart.refreshDashboard();
+    }).catch(function (e) {
+      utils.alert('error', 'Database Error', 'An error occurred while deleting the order : ' + e);
+      closeForm();
+    });
+  }
+  return {
+    setters: [function (_utils) {
+      utils = _utils;
+    }, function (_influx_helper) {
+      influx = _influx_helper;
+    }, function (_insertion_actions_ctrl) {
+      insert_actions = _insertion_actions_ctrl;
+    }, function (_editing_actions_ctrl) {
+      edit_actions = _editing_actions_ctrl;
+    }, function (_data_processor) {
+      dp = _data_processor;
+    }, function (_constans) {
+      cons = _constans;
+    }, function (_moment) {
+      moment = _moment.default;
+    }, function (_chart_option) {
+      chart = _chart_option;
+    }],
+    execute: function () {
+      _order = void 0;
+      _orderStates = void 0;
+
+      closeForm = function closeForm() {
+        return $('a#product-schedule-gantt-chart-order-actions-close-btn').trigger('click');
+      };
+    }
+  };
 });
 //# sourceMappingURL=order_actions_ctrl.js.map
